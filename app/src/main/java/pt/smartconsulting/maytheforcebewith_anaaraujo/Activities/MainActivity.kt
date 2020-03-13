2606db5226ee13ccfa7a9f5c2d747c92049cb958
anaraujo23 <anaraujo45@gmail.com>
2020-03-13 16:15:26 +0000

    MVVM Structure

---
+++ app/src/main/java/pt/smartconsulting/maytheforcebewith_anaaraujo/Activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package pt.smartconsulting.maytheforcebewith_anaaraujo.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pt.smartconsulting.maytheforcebewith_anaaraujo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
